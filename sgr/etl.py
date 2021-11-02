a586b6e1c77b8aac63b6f779a1ef06f68401350a
Ryder McMinn <mcminnra@gmail.com>
2021-11-01 22:10:09 -0700

    added model to main

--- sgr/etl.py
+++ sgr/etl.py
@@ -1,26 +1,31 @@
+from ast import literal_eval
+
 import pandas as pd
 from rich.progress import track
+from sentence_transformers import SentenceTransformer
 
 from steamapi import get_library_appids, get_wishlist_appids, get_store_data
 
-def get_data(df_input):
+def get_data(df):
+    total_games = df.shape[0]
+
     # Process scores df
-    df = df_input[df_input['Steam AppID'].notnull()]
+    df = df[df['Steam AppID'].notnull()]
     df = df.astype({'Steam AppID': int})
     df = df[['Steam AppID', 'Rating']]
-    print(f'Number of Reviewed Steam Games: {df.shape[0]}')
+    print(f'Number of Reviewed Steam Games: {df.shape[0]} (out of {total_games})')
 
     # Get library appids
     appids_library = get_library_appids()
     df_library = pd.DataFrame({'Steam AppID': appids_library, 'Rating':None})
     df_library = df_library.astype({'Steam AppID': int})
-    print(f'Number of Steam Games Found in Library: {df_library.shape[0]}')
+    print(f'Number of Games Found in Steam Library: {df_library.shape[0]}')
 
     # Get wishlist appids
     appids_wishlist = get_wishlist_appids()
     df_wishlist = pd.DataFrame({'Steam AppID': appids_wishlist, 'Rating':None})
     df_wishlist = df_wishlist.astype({'Steam AppID': int})
-    print(f'Number of Steam Games Found in Wishlist: {df_wishlist.shape[0]}')
+    print(f'Number of Games Found in Steam Wishlist: {df_wishlist.shape[0]}')
 
     # Join appids
     df = df.merge(df_library, on='Steam AppID', how='outer', suffixes=('', '_y'))[['Steam AppID', 'Rating']]
@@ -28,7 +33,7 @@ def get_data(df_input):
     df = df.set_index('Steam AppID')
 
     # Enrich appids
-    for appid in track(df.index.values, description='Enriching with Steam store data'):
+    for appid in track(df.index.values, description='Enriching with Steam Store Data'):
         data_appid = get_store_data(appid)
         keys = data_appid.keys()
 
@@ -36,5 +41,60 @@ def get_data(df_input):
             if key not in df.columns:
                 df[key] = None
             df.at[appid, key] = data_appid[key]
+
+    # Check for valid data points
+    before = df.shape[0]
+    df = df[df['name'].notnull()]
+    after = df.shape[0]
+    print(f'Pulled store data for {after} games ({before-after} games are missing store data).')
+
+    return df
+
+def preprocess_data(df):
+    df = df.set_index('Steam AppID')
+
+    # Fill Null
+    df['short_desc'] = df['short_desc'].fillna('')
+    df['long_desc'] = df['short_desc'].fillna('')
+
+    # Normalize ratings cols
+    df['recent_percent_norm']=(df['recent_percent']-df['recent_percent'].mean())/df['recent_percent'].std()
+    df['recent_count_norm']=(df['recent_count']-df['recent_count'].mean())/df['recent_count'].std()
+    df['all_percent_norm']=(df['all_percent']-df['all_percent'].mean())/df['all_percent'].std()
+    df['all_count_norm']=(df['all_count']-df['all_count'].mean())/df['all_count'].std()
+
+    # Explode Tags
+    df.tags = df.tags.apply(literal_eval)
+    df_tags = pd.get_dummies(df[['tags']].explode('tags')).sum(level=0)
+    df = df.merge(df_tags, how='inner', right_index=True, left_index=True)
+
+    ### Embeddings
+    model = SentenceTransformer('all-mpnet-base-v2')
+    model.max_seq_length = 300
+    # Process short_desc
+    # Get short_desc embeddings
+    df['short_desc_emb'] = None
+    for idx, sentence in zip(df.index, df['short_desc'].values):
+        sentence_emb = model.encode(sentence)
+        df.at[idx, 'short_desc_emb'] = sentence_emb
+
+    # Explode short_desc_emb to multiple cols
+    emb_len = df['short_desc_emb'].values[0].shape[0]
+    emb_cols = [f'short_desc_emb_{i}' for i in range(0, emb_len)]
+    df[emb_cols] = pd.DataFrame(df['short_desc_emb'].tolist(), index=df.index)
+    df = df.drop(['short_desc_emb'], axis=1)
+
+    # Process long_desc
+    # Get long_desc embeddings
+    df['long_desc_emb'] = None
+    for idx, sentence in zip(df.index, df['long_desc'].values):
+        sentence_emb = model.encode(sentence)
+        df.at[idx, 'long_desc_emb'] = sentence_emb
+
+    # Explode short_desc_emb to multiple cols
+    emb_len = df['long_desc_emb'].values[0].shape[0]
+    emb_cols = [f'long_desc_emb_{i}' for i in range(0, emb_len)]
+    df[emb_cols] = pd.DataFrame(df['long_desc_emb'].tolist(), index=df.index)
+    df = df.drop(['long_desc_emb'], axis=1)
 
     return df
--- sgr/sgr.py
+++ sgr/sgr.py
@@ -2,19 +2,24 @@
 
 import argparse
 import pathlib
+import pickle
 
+import numpy as np
 import pandas as pd
 from rich import print
 from rich.console import Console
+from sklearn.model_selection import GridSearchCV, cross_val_score
+from xgboost import XGBRegressor
 
-from etl import get_data
+from etl import get_data, preprocess_data
 
 # CLI
 parser = argparse.ArgumentParser(description='Steam Games Recommender')
 parser.add_argument(
     'reviews_filepath',
     type=str,
-    help='Reviews input filepath that has a "Steam AppID" and "Rating" columns.')
+    help='Reviews input filepath that has a "Steam AppID" and "Rating" columns.'
+)
 
 if __name__ == '__main__':
     # get print console
@@ -23,16 +28,88 @@ if __name__ == '__main__':
     # Get args
     args = parser.parse_args()
 
-    ### ETL    
+    ### ETL
     # Get reviewed games
     print(f'Reviews Input Path: {args.reviews_filepath}')
-    df_scores = pd.read_excel(args.reviews_filepath)
+    df = pd.read_excel(args.reviews_filepath)
 
     # Create data by getting library and wishlist games and enriching input
-    df = get_data(df_scores)
-    df.to_csv(str(pathlib.Path(__file__).parent.parent.absolute()) + '/data/data.csv')  # cache
+    df = get_data(df)
+    df.to_csv(str(pathlib.Path(__file__).parent.parent.absolute()) + '/data/raw.csv')  # cache
     del df
 
     ### Data Pre-processing
-    df = pd.read_csv(str(pathlib.Path(__file__).parent.parent.absolute()) + '/data/data.csv')
-    print(df)
+    df = pd.read_csv(str(pathlib.Path(__file__).parent.parent.absolute()) + '/data/raw.csv')
+    df = preprocess_data(df)
+    df.to_csv(str(pathlib.Path(__file__).parent.parent.absolute()) + '/data/processed.csv')  # cache
+    del df
+
+    ### Model Training
+    df = pd.read_csv(str(pathlib.Path(__file__).parent.parent.absolute()) + '/data/processed.csv').set_index('Steam AppID')
+    
+    normalized_cols = [col for col in df.columns if '_norm' in col]
+    tag_cols = [col for col in df.columns if 'tags_' in col]
+    embedding_cols = [col for col in df.columns if '_emb' in col]
+
+    df_train = df[df['Rating'].notnull()]
+    df_pred = df[df['Rating'].isnull()]
+
+    X_train = df_train[normalized_cols+tag_cols+embedding_cols]
+    X_pred = df_pred[normalized_cols+tag_cols+embedding_cols]
+    y_train = df_train['Rating']
+
+    # Fit Model
+    model = XGBRegressor(
+        max_depth=4,  # 32
+        n_estimators=354,  # 250
+        objective='reg:squarederror',
+        random_state=42,
+        verbosity=0,
+        n_jobs=-1)
+    model.fit(X_train, y_train)
+
+    # Get Cross Val Score
+    scores = cross_val_score(model, X_train, y_train, scoring='neg_mean_squared_error', cv=5)
+    print(f'Avg. MSE: {scores.mean():0.4f} (+/- {scores.std():0.4f})')
+
+    # Predict
+    y_pred = model.predict(X_pred)
+
+    df_pred = pd.DataFrame({
+        'AppID': X_pred.index.values,
+        'Pred Score': y_pred
+    }).sort_values('Pred Score', ascending=False).set_index('AppID')
+    df_pred = df_pred.join(df[['name']], how='left')
+    df_pred = df_pred[['name', 'Pred Score']]
+
+    # Top 10
+    print('\n== Top 10 ==')
+    for index, row in df_pred.head(10).iterrows():
+        print(f'{row["name"]}: {row["Pred Score"]:0.2f}')
+
+    # Bottom 10
+    print('\n== Bottom 10 ==')
+    for index, row in df_pred.tail(10).iterrows():
+        print(f'{row["name"]}: {row["Pred Score"]:0.2f}')
+
+    print('\n== Feature Importances ==')
+    feat_imp = [(col, imp) for col, imp in zip(X_train.columns, model.feature_importances_)]
+
+    tag_imp = np.mean([imp for col, imp in feat_imp if 'tags_' in col])
+    short_desc_imp = np.mean([imp for col, imp in feat_imp if 'short_desc_' in col])
+    long_desc_imp = np.mean([imp for col, imp in feat_imp if 'long_desc_' in col])
+
+    print(f'Tag Avg. Importance: {tag_imp:0.6f}')
+    print(f'Short Desc. Avg. Importance: {short_desc_imp:0.6f}')
+    print(f'Long Desc Avg. Importance: {long_desc_imp:0.6f}')
+    for col, imp in feat_imp:
+        if '_norm' in col:
+            print(f'{col} Importance: {imp:0.6f}')
+
+    print('\nTop 10 Tags')
+    tags = [(name, imp) for name, imp in feat_imp if 'tags_' in name]
+    for name, imp in sorted(tags, key=lambda x: x[1], reverse=True)[:10]:
+        print(name, imp)
+
+    pickle.dump(model, open(str(pathlib.Path(__file__).parent.parent.absolute()) + '/data/model.pkl', 'wb'))
+    
