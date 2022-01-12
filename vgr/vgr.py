733944f3db9e42e43f8eec8bd9ff438c4846a133
Ryder McMinn <mcminnra@gmail.com>
2022-01-11 19:41:53 -0800

    shap explanations

--- vgr/vgr.py
+++ vgr/vgr.py
@@ -6,9 +6,11 @@ import pathlib
 
 from hyperopt import fmin, hp, atpe, Trials, STATUS_OK
 import numpy as np
-from sklearn import metrics
+import pandas as pd
+import scipy.stats as stats
 from sklearn.decomposition import PCA
 from sklearn.model_selection import cross_val_score
+import shap
 from xgboost import XGBRegressor
 import yaml
 
@@ -87,7 +89,7 @@ if __name__ == '__main__':
     import warnings
     warnings.filterwarnings(action="ignore", message=r'.*Use subset.*of np.ndarray is not recommended')
 
-    # Hyperparam tuning
+    ### Train Model
     def objective(params):     
         pca = PCA(n_components=int(params['n_components']), random_state=42)
         model = XGBRegressor(
@@ -112,27 +114,24 @@ if __name__ == '__main__':
             'params': params
         }
 
-    search_space = {
-        'n_components': hp.quniform('n_components', 1, min(*X_train.shape), 1),
-        'max_depth': hp.quniform('max_depth', 3, 15, 1),  # Default = 6
-        'subsample': hp.uniform('subsample', 0.2, 1.0),
-        'colsample_bytree': hp.uniform('colsample_bytree', 0.2, 1.0),
-        'min_child_weight': hp.uniform('min_child_weight', 0, 2),
-        'reg_lambda': hp.uniform('reg_lambda', 0, 2),  # Default = 1
-        'reg_alpha': hp.uniform('reg_alpha', 0, 4),  # Default = 0
-    }
-
-    trials = Trials()  # allows us to record info from each iteration
     best = fmin(
         fn=objective,
-        space=search_space,
+        space={
+            'n_components': hp.quniform('n_components', 1, min(*X_train.shape), 1),
+            'max_depth': hp.quniform('max_depth', 3, 15, 1),  # Default = 6
+            'subsample': hp.uniform('subsample', 0.2, 1.0),
+            'colsample_bytree': hp.uniform('colsample_bytree', 0.2, 1.0),
+            'min_child_weight': hp.uniform('min_child_weight', 0, 2),
+            'reg_lambda': hp.uniform('reg_lambda', 0, 2),  # Default = 1
+            'reg_alpha': hp.uniform('reg_alpha', 0, 4),  # Default = 0
+        },
         algo=atpe.suggest,
         max_evals=1000,
-        trials=trials
+        trials=Trials()  # allows us to record info from each iteration
     )
     print(best)
 
-    # Train Model
+    # Use best params
     pca = PCA(n_components=int(best['n_components']), random_state=42)
     model = XGBRegressor(
         max_depth=int(best['max_depth']),
@@ -153,26 +152,100 @@ if __name__ == '__main__':
     scores = cross_val_score(model, X_train_pca, y_train, scoring='neg_mean_squared_error', cv=5)
     print(f'Avg. MSE: {scores.mean()*-1:0.4f} (+/- {scores.std():0.4f})')
 
-    # Predict
     y_pred = model.predict(pca.transform(X_pred))
-    
+
+    ### Train Shap Approx. Model
+    def objective_shap(params):     
+        model = XGBRegressor(
+            max_depth=int(params['max_depth']),
+            subsample=params['subsample'],
+            colsample_bytree=params['colsample_bytree'],
+            min_child_weight=params['min_child_weight'],
+            reg_lambda=params['reg_lambda'],
+            reg_alpha=params['reg_alpha'],
+            objective='reg:squarederror',
+            verbosity=0,
+            seed=42,
+            n_jobs=-1
+        )
+        model.fit(X_pred, y_pred)
+        loss = cross_val_score(model, X_pred, y_pred, scoring='neg_mean_squared_error', cv=5).mean()*-1
+
+        return {
+            'loss': loss,
+            'status': STATUS_OK,
+            'params': params
+        }
+
+    best = fmin(
+        fn=objective_shap,
+        space={
+            'max_depth': hp.quniform('max_depth', 3, 15, 1),  # Default = 6
+            'subsample': hp.uniform('subsample', 0.2, 1.0),
+            'colsample_bytree': hp.uniform('colsample_bytree', 0.2, 1.0),
+            'min_child_weight': hp.uniform('min_child_weight', 0, 2),
+            'reg_lambda': hp.uniform('reg_lambda', 0, 2),  # Default = 1
+            'reg_alpha': hp.uniform('reg_alpha', 0, 4),  # Default = 0
+        },
+        algo=atpe.suggest,
+        max_evals=15,
+        trials=Trials()  # allows us to record info from each iteration
+    )
+    print(best)
+
+    # Use best params
+    model_shap = XGBRegressor(
+        max_depth=int(best['max_depth']),
+        subsample=best['subsample'],
+        colsample_bytree=best['colsample_bytree'],
+        min_child_weight=best['min_child_weight'],
+        reg_lambda=best['reg_lambda'],
+        reg_alpha=best['reg_alpha'],
+        objective='reg:squarederror',
+        verbosity=0,
+        seed=42,
+        n_jobs=-1
+    )
+    model_shap.fit(X_pred, y_pred)
+    explainer = shap.TreeExplainer(model_shap)
+    shap_values = explainer(X_pred)
+    df_shap = pd.DataFrame(shap_values.values, columns=X_pred.columns)
+
+    corrs = []
+    for col in X_pred.columns:
+        corr, _ = stats.spearmanr(X_pred[col], df_shap[col])
+        corr = corr if not np.isnan(corr) else 0
+        corrs.append((col, corr))
+
+    c_i = [(col, corr, df_shap[col].abs().mean()) for col, corr in corrs]
+    c_i = sorted(c_i, key=lambda x: x[1]*x[2], reverse=True)
+    c_i = [(col, corr, shap_imp) for col, corr, shap_imp in c_i if 'text' not in col]  # Remove text embedding importance
+
+    ### Output
     df_pred['predicted_rating'] = y_pred
     df_pred = df_pred[['input_name', 'predicted_rating']].sort_values('predicted_rating', ascending=False)
 
-    # Top
-    print('\n== Top 25 ==')
+    # Games
+    print('\n== Top 25 Games ==')
     for index, row in df_pred.head(25).iterrows():
         print(f'{row["input_name"]}: {row["predicted_rating"]:0.2f}')
 
-    # Bottom
-    print('\n== Bottom 25 ==')
+    print('\n== Bottom 25 Games ==')
     for index, row in df_pred.tail(25).iterrows():
         print(f'{row["input_name"]}: {row["predicted_rating"]:0.2f}')
 
-    # Random
     print('\n == Random Game ==')
     rand_int = np.random.randint(low=0, high=df_pred.shape[0])
     print(f'{df_pred.iloc[rand_int]["input_name"]}: {df_pred.iloc[rand_int]["predicted_rating"]:0.2f}')
+
+    # Shap Corr+Magnitude
+    print('\n== Positive Features by Correlation+Importance ==')
+    for col, corr, shap_imp in c_i[:10]:
+        print(f'{col}: {corr:0.3f}//{shap_imp:0.3f}')
+
+    print('\n== Negative Features by Correlation+Importance ==')
+    for col, corr, shap_imp in c_i[-10:]:
+        print(f'{col}: {corr:0.3}//{shap_imp:0.3f}')
     
-    # Output
+    # Output File
     df_pred.to_csv(str(pathlib.Path(__file__).parent.parent.absolute()) + f'/data/scores_{today.year}_{today.month}_{today.day}.csv')
